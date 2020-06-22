1d4c8945c7009009da36f43373087cd5ba045035
BuildTools <okkeroelofsen@gmail.com>
2020-06-22 23:02:10 +0200

    -Player movement is now grid based -Added collisions -using every thing that is not a black tile as collider collision map is the same as tilemap - movement has cooldown (not sure bout this) - movement is on keydown event so you cant hold forward to go forward (bot sure about this either) think this is stable but need some testing otherwise can be merched to mainbranch

--- player.py
+++ player.py
@@ -6,73 +6,51 @@ class Player:
     def __init__(self, startlocation, speed, animspeed):
         self.pos = pygame.Vector2(startlocation)
         self.speed = speed
-        self.animspeed = animspeed
+        self.movecooldown = 0
         #   Player animations
         self.spr1 = pygame.image.load("entities\\player1.png")
         self.spr2 = pygame.image.load("entities\\player2.png")
-        self.animcount = 0
-        self.anim = 0
 
-    def move(self, dt, keyinput, map):
-        posch = self.movement(dt, keyinput)
-
-        if self.checkcollisions(self.pos+posch, map) == 1:
-            pos = self.pos+posch
-            if posch.x > 0:
-                self.pos.x = floor(pos[0]/8)-8
-            elif posch.x < 0:
-                self.pos.x = floor(pos[0]/8)+8
-            
-            if self.checkcollisions(self.pos+posch, map) == 1:
-                if posch.y > 0:
-                    self.pos.y = floor(pos[0]/8)-8
-                elif posch.y < 0:
-                    self.pos.y = floor(pos[0]/8)+8
+    def move(self, keyinput, map):
+        dir = self.movement(keyinput)
+        print(dir)
+        newpos = self.pos+dir
+        chunkx = floor(newpos.x/5)
+        chunky = floor(newpos.y/5)
+        x = newpos.x - chunkx*5
+        y = newpos.y - chunky*5
+        print(f"[{chunkx}.{chunky}][{int(y*5+x)}]")
+        if map[f"{chunkx}.{chunky}"][int(y*5+x)] == 0:
+            self.pos = newpos
         else:
-            self.pos += posch
+            print("collided")
 
-    def movement(self, dt, keyinput):
+    def movement(self, event):
         dir = pygame.Vector2(0, 0)
-        #   userinput
-        if keyinput[pygame.K_w]:
-            dir.y -= 1
-        if keyinput[pygame.K_s]:
-            dir.y += 1
-        if keyinput[pygame.K_a]:
-            dir.x -= 1
-        if keyinput[pygame.K_d]:
-            dir.x += 1
+        if self.movecooldown <= 0:
+            if event.key == pygame.K_w:
+                dir.y = -1
+            if event.key == pygame.K_s:
+                dir.y = 1
+            if event.key == pygame.K_a:
+                dir.x = -1
+            if event.key == pygame.K_d:
+                dir.x = 1
 
-        if dir != (0, 0):
-            dir.normalize_ip()
-        # change pos of player and collisionbox
-        return (dir * self.speed * dt)
+        if dir != [0, 0]:
+            self.movecooldown = self.speed
+        print(self.movecooldown)
+        return dir
 
-    def checkcollisions(self, pos, map):
-        chunkx = floor(floor(pos[0]/8)/25)
-        chunky = floor(floor(pos[1]/8)/25)
-        posx = floor(pos[0]/8) - chunkx*5
-        posy = floor(pos[1]/8) - chunky*5
-        if map[f"{chunkx}.{chunky}"][posx+posy*5] == 0:
-            return 0
-        elif map[f"{chunkx}.{chunky}"][posx+posy*5] == 1:
-            print("colliding")
-            return 1
-        return 2
+    def cooldown(self, dt):
+        self.movecooldown -= dt
 
     def draw(self, surface, dt, camerapos):
-        self.animcount += dt * self.animspeed
-        if self.animcount > 1:
-            self.animcount = 0
-            self.anim += 1
-            if self.anim > 1:
-                self.anim = 0
-
-        drawpos = self.pos
+        drawpos = self.pos*8
         drawpos.x - camerapos[0]
         drawpos.y - camerapos[1]
 
-        if self.anim == 0:
-            surface.blit(self.spr1, drawpos)
-        else:
-            surface.blit(self.spr2, drawpos)
+        #   if self.anim == 0:
+        surface.blit(self.spr1, drawpos)
+        #   else:
+        #    surface.blit(self.spr2, drawpos)
